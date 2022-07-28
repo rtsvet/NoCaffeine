d912cfa418def0a8be437a3027254de4a792e289
Rado Mitsu <rtzvetkov@yahoo.com>
2022-07-28 11:30:36 +0200

    .

--- No_Cafeine.R
+++ No_Cafeine.R
@@ -1,9 +1,44 @@
 # Caffeine 
 
 library(readr)
-oura <- read_csv("oura.csv", col_types = cols(Cafeine = col_integer(), 
-                                              date = col_date(format = "%Y-%m-%d"), 
-                                              `Week Day` = col_integer()))
-View(oura)
+
+##
+## caf <- read_csv("oura.csv", col_types = cols(Cafeine = col_integer(), 
+##                                              date = col_date(format = "%Y-%m-%d"), 
+##  `Week Day` = col_integer()))
+
+caf <- read.csv("oura.csv")
+View(caf)
+
+library(report)
+library(dplyr)
+library("ggplot2")
+library("GGally")
+
+grpDat <- group_by(caf, Caffeine)
+summarize(grpDat, 
+          meanDeep.Sleep = mean(Deep.Sleep.Duration),
+          meain.Total.Sleep = mean(Total.Sleep.Duration),
+          meanLoRHR = mean(Lowest.Resting.Heart.Rate),
+          meanAvHRV = mean(Average.HRV))
+
+# Let's try fisher test
+# H0: The two variables are independent.
+# H1: The two variables relate to each other.
+
+caf$DeepSleep.Stat <-as.factor(ifelse(caf$Deep.Sleep.Duration > mean(caf$Deep.Sleep.Duration), "inc", "dec"))
+caf$TotSleep.Stat <-as.factor(ifelse(caf$Total.Sleep.Duration > mean(caf$Total.Sleep.Duration), "inc", "dec"))
+
+tab.Deep.Sleep <- table(caf$Caffeine, caf$DeepSleep.Stat)
+names(dimnames(tab.Deep.Sleep)) <- c("Cafeine", "Deep Sleep ")
+tab.Deep.Sleep
+fisher.test(tab.Deep.Sleep)
 
 
+tab.Tot.Sleep <- table(caf$Caffeine, caf$TotSleep.Stat)
+names(dimnames(tab.Deep.Sleep)) <- c("Cafeine", "Tot Sleep ")
+tab.Tot.Sleep
+fisher.test(tab.Tot.Sleep)
+
+# We can NOT reject H0 - the variables might be independent
+# 
